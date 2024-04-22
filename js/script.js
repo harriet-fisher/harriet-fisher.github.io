37af6aa71ae927a230905145aecd7f5a4b6ef93b
Harriet Fisher <harrietfisher@uchicago.edu>
2024-04-22 15:59:33 -0500

    index

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Example: Toggle the recruitment status text on click
+document.getElementById('status-indicator').addEventListener('click', function() {
+    this.classList.toggle('not-looking');
+  });
+  
