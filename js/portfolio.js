bd15e10c93f0d8ae50aaa0825a61edffb0f3b678
harriet-fisher <harrietfisher@uchicago.edu>
2024-04-29 13:33:37 -0500

    portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,4 +1,28 @@
-app.get('/portfolio/:title', (req, res) => {
-    const projectTitle = req.params.title;
-    res.render('project-page', { title: projectTitle });
-});
+function switchTab(tabName) {
+    var tabs = document.querySelectorAll('.tab-content');
+    for (var i = 0; i < tabs.length; i++) {
+      tabs[i].style.display = 'none';
+    }
+  
+    var tabButtons = document.querySelectorAll('.tab');
+    for (var i = 0; i < tabButtons.length; i++) {
+      tabButtons[i].classList.remove('active');
+    }
+  
+    var activeTabContent = document.getElementById(tabName);
+    var activeTabButton = document.querySelector("[onclick=\"switchTab('" + tabName + "')\"]");
+
+    if (activeTabContent) {
+        activeTabContent.style.display = 'block';
+    }
+    
+    if (activeTabButton) {
+        activeTabButton.classList.add('active');
+        activeTabButton.focus(); // Focus on the active tab button
+    }
+}
+  
+  window.addEventListener('DOMContentLoaded', (event) => {
+    switchTab('tab1');
+  });
+  
