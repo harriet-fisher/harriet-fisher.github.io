cff951e764aaf3cd5a82fec6ae90f738470b2e6c
harriet-fisher <harrietfisher@uchicago.edu>
2024-10-18 16:43:11 -0500

    working relative sizing!

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var outputElement = document.getElementById('output');
   var inputElement = document.getElementById('command-input');
 
-  outputElement.innerHTML = '<img src="./assets/images/bubble_try.gif" class="lower-image" alt="About Image" style="width:500px">';
+  outputElement.innerHTML = '<img src="./assets/images/bubble_try.gif" class="lower-image" alt="About Image">';
 
 
   inputElement.addEventListener('keydown', function(event) {
@@ -11,19 +11,19 @@ document.addEventListener('DOMContentLoaded', function() {
       var response = '';
   
       if (command.toLowerCase() === 'commands') {
-        response = '<img src="./assets/images/bubble_commands.gif" class="lower-image" alt="About Image" style="width:500px">';
+        response = '<img src="./assets/images/bubble_commands.gif" class="lower-image" alt="About Image">';
       } else if (command.toLowerCase() === 'hello') {
-        response = '<img src="./assets/images/bubble_hello.gif" class="lower-image" alt="About Image" style="width:500px">';
+        response = '<img src="./assets/images/bubble_hello.gif" class="lower-image" alt="About Image">';
       } else if (command.toLowerCase() === 'hi') {
-        response = '<img src="./assets/images/bubble_hello.gif" class="lower-image" alt="About Image" style="width:500px">';
+        response = '<img src="./assets/images/bubble_hello.gif" class="lower-image" alt="About Image">';
       } else if (command.toLowerCase() === 'hey') {
-        response = '<img src="./assets/images/bubble_hello.gif" class="lower-image" alt="About Image" style="width:500px">';
+        response = '<img src="./assets/images/bubble_hello.gif" class="lower-image" alt="About Image">';
       } else if (command.toLowerCase() === 'about') {
-        response = '<img src="./assets/images/about_google_2.png" alt="About Image" style="width:970px; margin-left:-10px; margin-top:-10px">';
+        response = '<img src="./assets/images/about_google_2.png" alt="About Image">';
       } else if (command.toLowerCase() === 'help') {
-        response = '<img src="./assets/images/bubble_commands.gif" class="lower-image" alt="About Image" style="width:500px">';
+        response = '<img src="./assets/images/bubble_commands.gif" class="lower-image" alt="About Image">';
       } else if (command.toLowerCase() === 'facts') {
-        response = '<img src="./assets/images/facts_google.png" alt="About Image" style="height:450px; margin-left:-10px; margin-top:-10px">';
+        response = '<img src="./assets/images/facts_google.png" alt="About Image">';
       } else {
         response = 'Command not recognized.';
       }
